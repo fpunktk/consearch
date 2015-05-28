61f82da86c2eb6fd328e70ba2108f638cf922c47
Felix Kästner <git@fpunktk.de>
2015-05-28 21:01:51 +0200

    fix php in xml

--- opensearchdescription-consearch.xml.php
+++ opensearchdescription-consearch.xml.php
@@ -1,9 +1,15 @@
-<?xml version="1.0" encoding="UTF-8"?>
+<?php
+echo '<?xml version="1.0" encoding="UTF-8"?>
 <OpenSearchDescription xmlns="http://a9.com/-/spec/opensearch/1.1/">
 <ShortName>consearch</ShortName>
 <Description>conveniently use different searchengines</Description>
 <Tags>consearch</Tags>
 <Contact>consearch @ f p u n k t k . de</Contact>
-<Url type="text/html" template="https://www.fpunktk.de/consearch/index.php<?php if ( isset($_GET['dse']) and preg_match('/^[0-9a-z]{1,5}$/', $_GET['dse']) === 1 ) { echo "?dse=" . $_GET['dse']; } ?>#{searchTerms}" />
+<Url type="text/html" template="https://www.fpunktk.de/consearch/index.php';
+if ( isset($_GET['dse']) and preg_match('/^[0-9a-z]{1,5}$/', $_GET['dse']) === 1 ) {
+    echo '?dse=' . $_GET['dse'];
+}
+echo '#{searchTerms}" />
 <Image height="16" width="16" type="image/png">https://www.fpunktk.de/consearch/consearch.png</Image>
-</OpenSearchDescription>
+</OpenSearchDescription>';
+?>
